90525a1998d73e317c6fb50667d2d85f1aff2b80
Venu Sugguna <sugguna.venu@cognizant.com>
2020-02-02 00:26:07 +0530

    Updates

---
+++ precache-manifest.a817200000e786ea219cdbe89a989ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f272276d6900a22df9848fa94c44969",
+    "url": "/star-game/index.html"
+  },
+  {
+    "revision": "a5924c90484def23cd2a",
+    "url": "/star-game/static/css/main.77c73d81.chunk.css"
+  },
+  {
+    "revision": "db08edbeb090692448d9",
+    "url": "/star-game/static/js/2.343f35c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/star-game/static/js/2.343f35c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5924c90484def23cd2a",
+    "url": "/star-game/static/js/main.6071792d.chunk.js"
+  },
+  {
+    "revision": "f4b777e2aaea65bffc93",
+    "url": "/star-game/static/js/runtime-main.fdf40f15.js"
+  }
+]);
